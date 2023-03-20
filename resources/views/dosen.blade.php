4360e25d6e2cd9cee30d3d4352d18ccce36f23da
Burhanudin26 <113436431+Burhanudin26@users.noreply.github.com>
2023-03-20 22:52:48 +0700

    v8

--- resources/views/dosen.blade.php
+++ resources/views/dosen.blade.php
@@ -2,18 +2,24 @@
 
 @section('content')
     
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-    
-                    <div class="card-body" style="text-align: center">
-                        <h1>Ini adalah halaman dosen</h1>
-                        <button type="button" class="btn btn-primary">Attendance</button>
-                    </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+            <div class="card text-center">
+                <div class="card-header">
+                    
+                    <h1>Mata Kuliah</h1>
                 </div>
+                <a href="{{ url('presensi') }}" class="btn btn-primary" tabindex="-1" role="button" aria-pressed="true">Matematika Komputer</a>
+                <a href="{{ url('presensi') }}" class="btn btn-primary" tabindex="-1" role="button" aria-pressed="true">Komunikasi Data</a>
+                <a href="{{ url('presensi') }}" class="btn btn-primary" tabindex="-1" role="button" aria-pressed="true">Pemrograman Web</a>
+                <a href="{{ url('presensi') }}" class="btn btn-primary" tabindex="-1" role="button" aria-pressed="true">Probabilitas dan Stastika</a>
+                </div>
+
             </div>
         </div>
     </div>
+</div>
     
 @endsection
---
+++ storage/framework/views/a1ddb540e94d1ea04d3d5d3a42656052.php
@@ -0,0 +1,24 @@
+<?php $__env->startSection('content'); ?>
+    
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+            <div class="card text-center">
+                <div class="card-header">
+                    
+                    <h1>Mata Kuliah</h1>
+                </div>
+                <a href="<?php echo e(url('presensi')); ?>" class="btn btn-primary" tabindex="-1" role="button" aria-pressed="true">Matematika Komputer</a>
+                <a href="<?php echo e(url('presensi')); ?>" class="btn btn-primary" tabindex="-1" role="button" aria-pressed="true">Komunikasi Data</a>
+                <a href="<?php echo e(url('presensi')); ?>" class="btn btn-primary" tabindex="-1" role="button" aria-pressed="true">Pemrograman Web</a>
+                <a href="<?php echo e(url('presensi')); ?>" class="btn btn-primary" tabindex="-1" role="button" aria-pressed="true">Probabilitas dan Stastika</a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+    
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\Plus_Iso\CODE\WEB\TubesWeb\TUBES_WEB\resources\views/dosen.blade.php ENDPATH**/ ?>
