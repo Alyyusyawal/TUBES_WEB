636995b37b1aba214b478fdb8609eb9ac573336c
Burhanudin26 <113436431+Burhanudin26@users.noreply.github.com>
2023-03-20 19:58:27 +0700

    css dan js

--- storage/framework/views/d6c22799303ed746cfaa22dc28b3144b.php
+++ storage/framework/views/d6c22799303ed746cfaa22dc28b3144b.php
@@ -1,14 +1,14 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" >
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
+    <link href="<?php echo e(asset('assets/css/bootstrap.min.css')); ?>" rel="stylesheet" >
 
 
     <!-- Fonts -->
@@ -16,7 +16,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/sass/app.scss', 'resources/js/app.js']); ?>
 </head>
 <body>
     <style>
@@ -51,24 +51,24 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-primary shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="<?php echo e(url('/')); ?>">
                     LOGO
                 </a>
-                @auth
+                <?php if(auth()->guard()->check()): ?>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="{{url('admin')}}">Halaman Admin</a>
+                            <a class="nav-link" aria-current="page" href="<?php echo e(url('admin')); ?>">Halaman Admin</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{url('dosen')}}">Halaman Dosen</a>
+                            <a class="nav-link" href="<?php echo e(url('dosen')); ?>">Halaman Dosen</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="{{url('mahasiswa')}}">Halaman Mahasiswa</a>
+                            <a class="nav-link" href="<?php echo e(url('mahasiswa')); ?>">Halaman Mahasiswa</a>
                         </li>
                     </ul>
 
-                @endauth
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <?php endif; ?>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="<?php echo e(__('Toggle navigation')); ?>">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -81,37 +81,39 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
+                        <?php if(auth()->guard()->guest()): ?>
+                            <?php if(Route::has('login')): ?>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="<?php echo e(route('login')); ?>"><?php echo e(__('Login')); ?></a>
                                 </li>
-                            @endif
+                            <?php endif; ?>
 
-                            @if (Route::has('register'))
+                            <?php if(Route::has('register')): ?>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="<?php echo e(route('register')); ?>"><?php echo e(__('Register')); ?></a>
                                 </li>
-                            @endif
-                        @else
+                            <?php endif; ?>
+                        <?php else: ?>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                                    <?php echo e(Auth::user()->name); ?>
+
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                    <a class="dropdown-item" href="<?php echo e(route('logout')); ?>"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        <?php echo e(__('Logout')); ?>
+
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
+                                    <form id="logout-form" action="<?php echo e(route('logout')); ?>" method="POST" class="d-none">
+                                        <?php echo csrf_field(); ?>
                                     </form>
                                 </div>
                             </li>
-                        @endguest
+                        <?php endif; ?>
                     </ul>
                 </div>
             </div>
@@ -144,7 +146,7 @@
                   </ul></div>
               </div>
               <div class="col-6 bordered">
-                <div class="row bordered " rowspan="2" style="justify-content:center; height=100%">@yield('content')</div>
+                <div class="row bordered " rowspan="2" style="justify-content:center; height=100%"><?php echo $__env->yieldContent('content'); ?></div>
               </div>
               <div class="col-3 bordered">
                 <div class="row bordered" rowspan="2">
@@ -164,37 +166,7 @@
                   
               </div>
             </div>  
-            {{-- <div class="row">
-              <!--Kalender-->  
-              <div class="col-md-2">
-                <h2>Kalender</h2>
-                <iframe src="https://calendar.google.com/calendar/embed?src=example%40gmail.com&ctz=America%2FNew_York" style="border: 0" width="100%" height="600" frameborder="0" scrolling="no"></iframe>
-              </div>
-              <!--konten-->
-              <div class="col-8" rowspan="2">@yield('content')</div>
-              </div>
-              <!--panduan-->
-              <div class="col-2" rowspan="2">  
-                <h2>Panduan</h2>
-                <ul>
-                <li>List item 1</li>
-                <li>List item 2</li>
-                <li>List item 3</li>
-              </ul>
-            </div>
-            </div>
-            <div class="row">
-                <div class="col-6 col-md-4"></div>
-                <div class="col-6 col-md-4"></div>
-                <!--helpdesk-->
-                <div class="col-6 col-md-4">
-                    <ul>
-                    <h2>helpdesk</h2>
-                      <li>List item 1</li>
-                      <li>List item 2</li>
-                      <li>List item 3</li>
-                    </ul>
-            </div> --}}
+            
           </main>
           
     </div>
@@ -206,3 +178,4 @@
     </div>
 </body>
 </html>
+<?php /**PATH E:\Plus_Iso\CODE\WEB\TubesWeb\TUBES_WEB\resources\views/layouts/app.blade.php ENDPATH**/ ?>
